54cb3e6a359e224a24839a094d74170615a73780
Yangxg <jessebutchman@gmail.com>
2016-06-30 02:49:44 +0800

    终于跑起来了

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,30 @@
-import thunkMiddleware from 'redux-thunk'
-import createLogger from 'redux-logger'
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// import thunkMiddleware from 'redux-thunk'
+// import createLogger from 'redux-logger'
+// const loggerMiddleware = createLogger()
 import { createStore, applyMiddleware } from 'redux'
 import { selectSubreddit, fetchPosts } from './actions/action.js'
 import rootReducer from './reducers/reducer.js'
 
+import configStore from './store/configureStore.js';
+
+import App from './container/Root.js';
+
 // require('file?name=index.html!../index.html');
 require("file?name=index.html!../index.html");
 
-const loggerMiddleware = createLogger()
+const store = configStore();
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware, // lets us dispatch() functions
-    loggerMiddleware // neat middleware that logs actions
-  )
-)
+console.log(store);
 
 store.dispatch(selectSubreddit('reactjs'))
-// store.dispatch(fetchPosts('reactjs')).then(() =>
-//   console.log(store.getState())
-// )
 
 store.dispatch(fetchPosts('reactjs')).then(() =>
   console.log(store.getState())
+)
+ReactDom.render(
+    <App store={store} />,
+    document.getElementById('app')
 )
--- src/js/store/configureStore.dev.js
+++ src/js/store/configureStore.dev.js
@@ -1,13 +1,20 @@
 import {createStore,applyMiddleware,compose} from 'redux';
 import {persistState} from 'redux-devtools';
-import rootReducer from '../reducer';
+import rootReducer from '../reducers/reducer.js';
 import DevTools from '../container/DevTools.jsx';
 
+import thunkMiddleware from 'redux-thunk'
+import createLogger from 'redux-logger'
+const loggerMiddleware = createLogger()
 
 const enhancer = compose(
-    applyMiddleware(),
+      applyMiddleware(
+        thunkMiddleware, // lets us dispatch() functions
+        loggerMiddleware // neat middleware that logs actions
+      ),
     DevTools.instrument(),
-    persistState(getDebugSessionKey())
+    persistState(getDebugSessionKey()),
+
 );
 
 function getDebugSessionKey(){
